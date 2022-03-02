7ced623c965d9e6059d3339d588f4fa1c33f1ffd
diptam1234 <82029131+diptam1234@users.noreply.github.com>
2022-03-02 10:27:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {  View} from 'react-native';
+
+import PredictImage from './screens/camera';
+
+
+export default function App() {
+  return (
+    <View >
+      <PredictImage/>
+    </View>
+  );
+}
